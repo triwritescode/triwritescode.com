00bd730f69e8f896d8a619d14a7471f700ff1e08
Tri Denda <triwritescode@gmail.com>
2026-02-24 13:56:25 +0700

    installed speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Analytics as VercelAnalytics } from "@vercel/analytics/next";
+import { SpeedInsights as VercelSpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -31,6 +32,7 @@ export default function RootLayout({
       >
         {children}
         <VercelAnalytics />
+        <VercelSpeedInsights />
       </body>
     </html>
   );
